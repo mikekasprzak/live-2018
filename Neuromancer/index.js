87be7711b86f88b7e0fc0b4a261179ad52ca808b
Mike Kasprzak <mikekasprzak@gmail.com>
2017-12-31 03:56:26 -0500

    added flashing cursor

---
+++ Neuromancer/index.js
@@ -0,0 +1,25 @@
+
+
+var Data = [
+	{
+		'name': "CASE",
+		'class': "cyan",
+		'child': [
+			["BAMA:", "gray"],
+			["1234567890"]
+		]
+	},
+	{
+		'name': "DATABASE",
+		'class': "skin",
+		'child': [
+			["Cheap Hotel:", "gray"],
+			["CHEAPO"],
+			["GUEST [lvl 1]"],
+			["COCKROACH [lvl 2]"],
+			[],
+		]
+	}
+
+];
+
