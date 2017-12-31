3848ea5e5fca91aab81e0400b51e09aff0ac8cdc
Mike Kasprzak <mikekasprzak@gmail.com>
2017-12-31 04:52:24 -0500

    using obs-websocket lib

--- remote/index.js
+++ remote/index.js
@@ -1,39 +1,50 @@
 
-const WS_READYSTATE_CONNECTING = 0;
-const WS_READYSTATE_OPEN = 1;
-const WS_READYSTATE_CLOSING = 2;
-const WS_READYSTATE_CLOSED = 3;
-
-
-var sock = new WebSocket("ws://localhost:4444");
-function send( data ) {
-	sock.send(JSON.stringify(data));
-}
-
-
-sock.addEventListener('open', function( event ) {
-	console.log( event );
-
- 	send({
-		'request-type': "GetVersion",
-		'message-id': "GetVersion",
- 	});
+// https://github.com/haganbmj/obs-websocket-js
+const obs = new OBSWebSocket();
+obs.connect({address: 'localhost:4444'})
+.then(r => {
+	return obs.GetVersion();
+})
+.then(r => {
+	console.log(r);
 });
 
-sock.onmessage = function( event ) {
-	var data = JSON.parse(event.data);
 
-	if ( data['message-id'] )
-		console.log('RESPONSE:', data['message-id'], data);
-	else
-		console.log('EVENT:', data['update-type'], data);
-
-	if ( data['message-id'] == "GetVersion" ) {
-		send({
-			'request-type': "ListProfiles",
-			'message-id': "ListProfiles",
-		});
-	}
-}
+//const WS_READYSTATE_CONNECTING = 0;
+//const WS_READYSTATE_OPEN = 1;
+//const WS_READYSTATE_CLOSING = 2;
+//const WS_READYSTATE_CLOSED = 3;
+//
+//
+//var sock = new WebSocket("ws://localhost:4444");
+//function send( data ) {
+//	sock.send(JSON.stringify(data));
+//}
+//
+//
+//sock.addEventListener('open', function( event ) {
+//	console.log( event );
+//
+// 	send({
+//		'request-type': "GetVersion",
+//		'message-id': "GetVersion",
+// 	});
+//});
+//
+//sock.onmessage = function( event ) {
+//	var data = JSON.parse(event.data);
+//
+//	if ( data['message-id'] )
+//		console.log('RESPONSE:', data['message-id'], data);
+//	else
+//		console.log('EVENT:', data['update-type'], data);
+//
+//	if ( data['message-id'] == "GetVersion" ) {
+//		send({
+//			'request-type': "ListProfiles",
+//			'message-id': "ListProfiles",
+//		});
+//	}
+//}
 
 
