125d36724fcc26709ce03a4ca6c8d7187aadea18
Mike Kasprzak <mikekasprzak@gmail.com>
2018-01-03 04:57:50 -0500

    now all propr

--- remote/src/main.js
+++ remote/src/main.js
@@ -1,4 +1,6 @@
 import {h, render, Component}			from 'preact/preact';
+import UIIcon							from 'ui/icon/icon';
+import UIImage							from 'ui/image/image';
 
 // @ifdef DEBUG
 //import {}								from 'preact-devtools/devtools';
@@ -19,7 +21,13 @@ class Main extends Component {
 	render( props ) {
 		//document.title = this.getTitle(node);
 
-		return <div>Hey</div>;
+		return (
+			<div>
+				<div>Hey</div>
+				<UIIcon>radio-empty</UIIcon>
+				<UIImage src="../common/ChickenC64.png" />
+			</div>
+		);
 	}
 }
 
