78b10fdbbc3321790f40bc06d62bfa8015067a3c
Mike Kasprzak <mikekasprzak@gmail.com>
2018-01-06 02:10:54 -0500

    Link is now compatible, as is main

--- remote/src/main.js
+++ remote/src/main.js
@@ -1,6 +1,7 @@
 import {h, render, Component}			from 'preact/preact';
 import UIIcon							from 'ui/icon/icon';
 import UIImage							from 'ui/image/image';
+import UILink							from 'ui/link/link';
 
 // @ifdef DEBUG
 //import {}								from 'preact-devtools/devtools';
@@ -10,7 +11,6 @@ import UIImage							from 'ui/image/image';
 class Main extends Component {
 	constructor( props ) {
 		super(props);
-		console.log("[constructor]");
 		// @ifdef DEBUG
 		console.log("Running in DEBUG mode");
 		// @endif
@@ -20,14 +20,52 @@ class Main extends Component {
 		};
 
 		window.addEventListener('hashchange', this.hashChange.bind(this));
+		window.addEventListener('navchange', this.navChange.bind(this));
+	}
+
+	componentWillMount() {
+		this.hashChange();
 	}
 
 	hashChange( e ) {
+		if ( window.location.href.indexOf('#!/') === -1 ) {
+			// Assume if no variables are set as part of the URL, we should keep the old ones
+			var Vars = window.location.search;
+			if ( !Vars ) {
+				if ( window.location.hash.indexOf('?') !== -1 ) {
+					Vars = '?' + (window.location.hash.split('?'))[1];
+				}
+			}
+			history.replaceState(null, null, window.location.pathname + '#!/' + Vars);
+			this.setState({'url': parseHashBang()});
+		}
+
 		this.setState({'url': parseHashBang()});
+		this.onChange(e);
+	}
+
+	navChange( e ) {
+		if ( e.detail.location.href !== e.detail.old.href ) {
+			// Assume if no variables are set as part of the URL, we should keep the old ones
+			var Vars = e.detail.location.search;
+			if ( !Vars ) {
+				if ( e.detail.old.hash.indexOf('?') !== -1 ) {
+					Vars = '?' + (e.detail.old.hash.split('?'))[1];
+				}
+			}
+			history.pushState(null, null, e.detail.location.pathname + e.detail.location.hash + Vars);
+
+			this.setState({'url': parseHashBang()});
+			this.onChange(e);
+		}
+	}
+
+	onChange( e ) {
+		window.scrollTo(0, 0);
 	}
 
 	render( props, state ) {
-		//document.title = this.getTitle(node);
+		document.title = state.url.pathname;
 
 		console.log(state.url);
 
@@ -36,6 +74,7 @@ class Main extends Component {
 				<div>Hey</div>
 				<UIIcon>checkbox-checked</UIIcon>
 				<UIImage src="../common/ChickenC64.png" />
+				<div><UILink href="#!/">null</UILink>, <UILink href="#!/puber">puber</UILink>, <UILink href="#!/shemba">shemba</UILink></div>
 			</div>
 		);
 	}
