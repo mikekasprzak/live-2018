4e1e9beca9d3aab98dbb365f0def0140b65b761f
Mike Kasprzak <mikekasprzak@gmail.com>
2018-01-19 22:01:42 -0500

    added more socials

--- remote/src/view-chat/view-chat.js
+++ remote/src/view-chat/view-chat.js
@@ -9,6 +9,9 @@ export default class ViewChat extends Component {
 
 		this.state = {
 			'twitch': null,
+			'mixer': null,
+			'smashcast': null,
+			'twitter': null,
 		};
 	}
 
@@ -20,13 +23,23 @@ export default class ViewChat extends Component {
 		);
 	}
 
+	renderStatus( status ) {
+		return status ? <UIIcon class="on" src="checkmark" small /> : <UIIcon class="off" src="cross" small />;
+	}
+
 	render( props, state ) {
-		const TwitchStatus = state.twitch ? <UIIcon class="on" src="checkmark" small /> : <UIIcon class="off" src="cross" small />;
+		const TwitchStatus = this.renderStatus(state.twitch);
+		const MixerStatus = this.renderStatus(state.mixer);
+		const SmashCastStatus = this.renderStatus(state.smashcast);
+		const TwitterStatus = this.renderStatus(state.twitter);
 
 		return (
 			<div id="chat">
 				<div class="info">
 					<div><span><UIIcon src="twitch" />:</span>{TwitchStatus}</div>
+					<div><span><UIIcon src="mixer" />:</span>{MixerStatus}</div>
+					<div><span><UIIcon src="smashcast" />:</span>{SmashCastStatus}</div>
+					<div><span><UIIcon src="twitter" />:</span>{TwitterStatus}</div>
 				</div>
 				<div class="feed">
 					{this.renderItem("David", "Are you my daddy?", 'twitch')}
--- remote/src/view-main/view-main.js
+++ remote/src/view-main/view-main.js
@@ -13,15 +13,26 @@ export default class ViewMain extends Component {
 			'youtube': null,
 			'mixer': null,
 			'smashcast': null,
+			'twitter': null,
+			'facebook': null,
+			'instagram': null,
 		};
 	}
 
+
+	renderStatus( status ) {
+		return status ? <UIIcon class="on" src="checkmark" small /> : <UIIcon class="off" src="cross" small />;
+	}
+
 	render( props, state ) {
-		const OBSStatus = state.obs ? <UIIcon class="on" src="checkmark" small /> : <UIIcon class="off" src="cross" small />;
-		const TwitchStatus = state.twitch ? <UIIcon class="on" src="checkmark" small /> : <UIIcon class="off" src="cross" small />;
-		const YouTubeStatus = state.youtube ? <UIIcon class="on" src="checkmark" small /> : <UIIcon class="off" src="cross" small />;
-		const MixerStatus = state.mixer ? <UIIcon class="on" src="checkmark" small /> : <UIIcon class="off" src="cross" small />;
-		const SmashCastStatus = state.smashcast ? <UIIcon class="on" src="checkmark" small /> : <UIIcon class="off" src="cross" small />;
+		const OBSStatus = this.renderStatus(state.obs);
+		const TwitchStatus = this.renderStatus(state.twitch);
+		const YouTubeStatus = this.renderStatus(state.youtube);
+		const MixerStatus = this.renderStatus(state.mixer);
+		const SmashCastStatus = this.renderStatus(state.smashcast);
+		const TwitterStatus = this.renderStatus(state.twitter);
+		const FacebookStatus = this.renderStatus(state.facebook);
+		const InstagramStatus = this.renderStatus(state.instagram);
 
 		return (
 			<div id="main">
@@ -31,6 +42,9 @@ export default class ViewMain extends Component {
 					<div><span><UIIcon src="youtube" />:</span>{YouTubeStatus}</div>
 					<div><span><UIIcon src="mixer" />:</span>{MixerStatus}</div>
 					<div><span><UIIcon src="smashcast" />:</span>{SmashCastStatus}</div>
+					<div><span><UIIcon src="twitter" />:</span>{TwitterStatus}</div>
+					<div><span><UIIcon src="facebook" />:</span>{FacebookStatus}</div>
+					<div><span><UIIcon src="instagram" />:</span>{InstagramStatus}</div>
 				</div>
 				<div class="body">
 					<div class="flex">
